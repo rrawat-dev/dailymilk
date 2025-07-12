2faa147494d0ce1e16fef90d8e1ed07a2a867abd
Rakesh Rawat <rakrawat@WKMZT038C187.global.publicisgroupe.net>
2025-07-12 21:33:39 +0530

    Remove appwrite auth files

--- lib/appwrite.ts
+++
@@ -1,9 +0,0 @@
-import { Account, Client } from 'react-native-appwrite';
-
-const client = new Client()
-  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
-  .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
-
-
-export const account = new Account(client);
